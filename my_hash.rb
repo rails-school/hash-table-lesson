79b51dc4dd6df9f224b8ee93aae38b17cb3fd293
Gabe Kopley <gabe@coshx.com>
2015-03-16 16:41:31 -0700

    Initial commit

---
+++ my_hash.rb
@@ -0,0 +1,50 @@
+class MyHash
+  def initialize
+    @size  = 10
+    @array = []
+  end
+
+  def [](key)
+    kv_pair = @array[slot_number(key)]
+    kv_pair[1] if kv_pair
+  end
+
+  def []=(key, value)
+    n = slot_number(key)
+    @array[n] = [key, value]
+  end
+
+  private
+  def hash(object)
+    object.hash % @size
+  end
+
+  def resize
+    old_array = @array
+    @array = []
+    @size *= 2
+    old_array.each do |k, v|
+      self[k] = v
+    end
+  end
+
+  def slot_number(key)
+    initial_slot_number = slot_number = hash(key)
+    loop do
+      kv_pair = @array[slot_number]
+
+      if !kv_pair || kv_pair[0] == key
+        # Either this slot belongs to the key, or the slot is free
+        return slot_number
+      end
+
+      slot_number = (slot_number + 1) % @size
+
+      if slot_number == initial_slot_number
+        # Uh oh, we wrapped around to where we started
+        resize
+        initial_slot_number = slot_number = hash(key)
+      end
+    end
+  end
+end
---
+++ my_hash_spec.rb
@@ -0,0 +1,46 @@
+require "./my_hash"
+require "pry"
+
+RSpec.describe MyHash do
+  it "sets and retrieves some simple keys and values" do
+    expect(subject["foo"]).to be_nil
+
+    subject["foo"] = 3
+    expect(subject["foo"]).to eq(3)
+
+    subject[:bar] = {bop: {}}
+    expect(subject[:bar]).to eq({bop: {}})
+    expect(subject["foo"]).to eq(3)
+
+    subject[3] = "three"
+    expect(subject[:bar]).to eq({bop: {}})
+    expect(subject["foo"]).to eq(3)
+    expect(subject[3]).to eq("three")
+  end
+
+  it "handles collisions" do
+    expect(subject).to receive(:hash).with(3).and_return(5).at_least(:once)
+    expect(subject).to receive(:hash).with(13).and_return(5).at_least(:once)
+
+    subject[3] = "three"
+    expect(subject[3]).to eq("three")
+
+    subject[13] = "thirteen"
+    expect(subject[3]).to eq("three")
+    expect(subject[13]).to eq("thirteen")
+  end
+
+  it "resizes automatically when it runs out of space" do
+    expect(subject.instance_variable_get("@size")).to eq(10)
+
+    11.times do |i|
+      subject[i] = i.to_s
+    end
+
+    11.times do |i|
+      expect(subject[i]).to eq(i.to_s)
+    end
+
+    expect(subject.instance_variable_get("@size")).to eq(20)
+  end
+end
